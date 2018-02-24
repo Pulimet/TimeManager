922392f87439bfe8a52b55eb1aa3e15a6aecded4
Pulimet <korolev2009@gmail.com>
2018-02-24 22:00:29 +0200

    Regular login

--- app/src/main/java/net/alexandroid/utils/apollotest/LoginActivity.java
+++ app/src/main/java/net/alexandroid/utils/apollotest/LoginActivity.java
@@ -4,29 +4,95 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
 
+import com.AuthFBMutation;
+import com.AuthLocalMutation;
+import com.apollographql.apollo.ApolloCall;
+import com.apollographql.apollo.ApolloClient;
+import com.apollographql.apollo.api.Error;
+import com.apollographql.apollo.api.Response;
+import com.apollographql.apollo.exception.ApolloException;
 import com.facebook.AccessToken;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
+import com.type.AuthLocalInput;
 
+import net.alexandroid.shpref.ShPref;
 import net.alexandroid.utils.mylog.MyLog;
 
 import java.util.Arrays;
 
+import javax.annotation.Nonnull;
+
+import okhttp3.OkHttpClient;
+
 public class LoginActivity extends AppCompatActivity implements FacebookCallback<LoginResult> {
 
     private CallbackManager mCallbackManager;
+    private ApolloClient mApolloClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        mApolloClient = ApolloClient.builder()
+                .serverUrl("http://104.196.187.249:3000/api/")
+                .okHttpClient(new OkHttpClient())
+                .build();
+
         checkIfLoggedIn();
+        setRegularLogin();
         setFbLogin();
+    }
+
+    private void setRegularLogin() {
+        findViewById(R.id.regularLogin).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View pView) {
+
+                AuthLocalInput authLocalInput = AuthLocalInput.builder()
+                        .email("test@korolev.co.il")
+                        .firstName("Alesha")
+                        .lastName("Korolev")
+                        .password("q1w2e3r4")
+                        .build();
+
+                AuthLocalMutation authLocalMutation = AuthLocalMutation.builder()
+                        .user(authLocalInput)
+                        .isSignUp(false)
+                        .build();
+
+
+                mApolloClient.mutate(authLocalMutation)
+                        .enqueue(new ApolloCall.Callback<AuthLocalMutation.Data>() {
+                            @Override
+                            public void onResponse(@Nonnull Response<AuthLocalMutation.Data> response) {
+                                if (response.hasErrors()) {
+                                    for (Error error : response.errors()) {
+                                        MyLog.i(error.message());
+                                    }
+                                }
+                                if (response.data() != null) {
+                                    MyLog.d(response.data().authLocal().firstName());
+                                } else {
+                                    MyLog.e("data is null");
+                                }
+                                startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                                finish();
+                            }
+
+                            @Override
+                            public void onFailure(@Nonnull ApolloException e) {
+
+                            }
+                        });
+            }
+        });
     }
 
     private void checkIfLoggedIn() {
@@ -41,7 +107,7 @@ public class LoginActivity extends AppCompatActivity implements FacebookCallback
 
     private void setFbLogin() {
         mCallbackManager = CallbackManager.Factory.create();
-        LoginButton loginButton = findViewById(R.id.login_button);
+        LoginButton loginButton = findViewById(R.id.fbLogin);
         loginButton.setReadPermissions(Arrays.asList("public_profile"));
         // Callback registration
         loginButton.registerCallback(mCallbackManager, this);
@@ -57,12 +123,12 @@ public class LoginActivity extends AppCompatActivity implements FacebookCallback
 
     @Override
     public void onCancel() {
-        Log.d("ZAQ", "onCancel");
+        MyLog.d("onCancel");
     }
 
     @Override
     public void onError(FacebookException exception) {
-        Log.e("ZAQ", "onError");
+        MyLog.e("onError, " + exception.getMessage());
     }
 
     // Facebook end
@@ -74,10 +140,34 @@ public class LoginActivity extends AppCompatActivity implements FacebookCallback
     }
 
     private void startMainActivity(String pToken) {
+        ShPref.put(R.string.fb_token, pToken);
+        AuthFBMutation authFBMutation = AuthFBMutation.builder()
+                .session(pToken)
+                .build();
 
+        mApolloClient.mutate(authFBMutation)
+                .enqueue(new ApolloCall.Callback<AuthFBMutation.Data>() {
+                    @Override
+                    public void onResponse(@Nonnull Response<AuthFBMutation.Data> response) {
+                        if (response.hasErrors()) {
+                            for (Error error : response.errors()) {
+                                MyLog.i(error.message());
+                            }
+                        }
+                        if (response.data() != null) {
+                            MyLog.d(response.data().authFB().firstName());
+                        } else {
+                            MyLog.e("data is null");
+                        }
+/*                        startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                        finish();*/
+                    }
 
-/*        ShPref.put(R.string.fb_token, pToken);
-        startActivity(new Intent(this, MainActivity.class));
-        finish();*/
+                    @Override
+                    public void onFailure(@Nonnull ApolloException e) {
+
+                    }
+                });
+
     }
 }
--- app/src/main/java/net/alexandroid/utils/apollotest/MainActivity.java
+++ app/src/main/java/net/alexandroid/utils/apollotest/MainActivity.java
@@ -7,9 +7,12 @@ import android.util.Log;
 import com.TasksQuery;
 import com.apollographql.apollo.ApolloCall;
 import com.apollographql.apollo.ApolloClient;
+import com.apollographql.apollo.api.Error;
 import com.apollographql.apollo.api.Response;
 import com.apollographql.apollo.exception.ApolloException;
 import com.type.TasksSearchInput;
+
+import net.alexandroid.utils.mylog.MyLog;
 
 import java.util.List;
 
@@ -39,15 +42,21 @@ public class MainActivity extends AppCompatActivity {
                 .enqueue(new ApolloCall.Callback<TasksQuery.Data>() {
                     @Override
                     public void onResponse(@Nonnull Response<TasksQuery.Data> response) {
-                        Log.d("ZAQ", "Response");
+                        MyLog.d( "Response");
+                        if (response.hasErrors()) {
+                            for (Error error : response.errors()) {
+                                MyLog.i(error.message());
+                            }
+                        }
+
                         TasksQuery.Data data = response.data();
                         if (data != null) {
                             List<TasksQuery.Task> list = data.tasks();
                             for (TasksQuery.Task task : list) {
-                                Log.d("ZAQ", "" + task.title() + " - " + task);
+                                MyLog.d( "" + task.title() + " - " + task);
                             }
                         } else {
-                            Log.d("ZAQ", "Data null");
+                            MyLog.d( "Data null");
                         }
                     }
 
